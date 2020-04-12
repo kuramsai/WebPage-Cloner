485dbd87737e88792a57056b8ccf4f94264e8361
Sai Kiran <kuramsai@yahoo.com>
2020-04-12 16:45:41 +0530

    update to get images and fonts in css

--- CloneWebPage_3.py
+++ CloneWebPage_3.py
@@ -2,14 +2,15 @@
 #place link of the website without index.html
 #eg: http://xyz.com/index.html is the website you want to clone
 #put the base URL as http://xyz.com/
-baseurl = 'Replace this'
+baseurl = 'Replace This'
 
 from bs4 import BeautifulSoup
 import os
 from urllib.request import urlretrieve
 from urllib.request import urlretrieve
 import urllib.request
-
+import cssutils
+import logging
 
 
 print ('''Python script to Clone a Web Page
@@ -26,6 +27,7 @@ def report(count, size, total):
             print("Downloaded {:,}/{:,} ...".format(progress[1], total))
 
 print ("Connecting to server")
+cssutils.log.setLevel(logging.CRITICAL)
 
 opener = urllib.request.build_opener()
 #defining headers as some servers mandiate it
@@ -34,7 +36,7 @@ opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit
                         ('Connection', 'keep-alive')
                     ]
 urllib.request.install_opener(opener)
-html_doc = urllib.request.urlopen(baseurl).read()
+html_doc = urllib.request.urlopen('https://envytheme.com/tf-demo/crake/crake/index-8.html').read()
 
 print ("Connection Success!")
 try :
@@ -49,7 +51,7 @@ try :
         a = soup.find_all('img')
         for i in range(len(a)):
             directory =  a[i]['src']
-            print ('\t[+]Getting file = '+str(directory))
+            print ('\t[+]Getting img = '+str(directory))
             if not os.path.exists(os.path.dirname(directory)):
                 print ("    [DIR]Creating directory")
                 os.makedirs(os.path.dirname(directory))
@@ -60,16 +62,40 @@ try :
         a = soup.find_all('link')
         for i in range(len(a)):
             directory =  a[i]['href']
+            if(".css" not in directory):
+                print("-------Skipped for ---------",directory)
+                continue
             if "http" in directory or "https" in directory:
                 print ("------Skipped for ----- ",directory)
                 continue
-            print ('\t[+]Getting file = '+str(directory))
+            print ('\t[+]Getting CSS = '+str(directory))
             if "/" not in directory:
                     print ("\tNo directory. Saving file",directory)
             elif not os.path.exists(os.path.dirname(directory)):
                 print ("    [DIR]Creating directory")
                 os.makedirs(os.path.dirname(directory))
-                testfile, headers = urlretrieve(baseurl+directory, directory, reporthook=report)
+            testfile, headers = urlretrieve(baseurl+directory, directory, reporthook=report)   
+            urls = list( cssutils.getUrls(cssutils.parseFile(directory)))
+            if(len(urls)!=0):
+                for link in urls:
+                    try:
+                        if "http" in directory or "https" in link or "data:image/" in link:
+                            print ("------Skipped for ----- ",link)
+                            continue
+                        while("../" in link):
+                            if("assets" in link):
+                                link = link[3:]
+                            else:
+                                link = "assets/"+link[3:]
+                        print ('\t\t[+]Getting CSS-Image = '+str(link))
+                        if "/" not in link:
+                                print ("\t\tNo directory. Saving file",link)
+                        elif not os.path.exists(os.path.dirname(link)):
+                            print ("    [DIR]Creating directory")
+                            os.makedirs(os.path.dirname(link))
+                        testfile, headers = urlretrieve(baseurl+link, link, reporthook=report)
+                    except Exception as e:
+                        print ("Excpetion occurred in CSS-Inner for",e)
         print ('==============Done getting CS files!==============')
         print ("Step 3: Getting all JS.")
         #Get all JS
@@ -78,17 +104,17 @@ try :
             try:
                 directory =  a[i]['src']
             except Exception as e:
-                print ("Excpetion occured in JS for",a[i])
+                print ("Excpetion occurred in JS for",a[i])
                 continue
             if "http" in directory or "https" in directory:
                 print ("------Skipped for ----- ",directory)
                 continue
-            print ('\t[+]Getting file = '+str(directory))
+            print ('\t[+]Getting JS = '+str(directory))
             if not os.path.exists(os.path.dirname(directory)):
                 print ("    [DIR]Creating directory")
                 os.makedirs(os.path.dirname(directory))
-                testfile, headers = urlretrieve(baseurl+directory, directory, reporthook=report)
+            testfile, headers = urlretrieve(baseurl+directory, directory, reporthook=report)
         print ('==============Done getting JS Files!==============')
-        print ('Script Executed sucessfully!')
+        print ('Script Executed successfully!')
 except Exception as e:
-    print ("Exception occured = ",e)
+    print ("Exception occurred = ",e)
